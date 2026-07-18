458fba0e116614de335c76157ff327714f084464
Arek <132752926+ohsofreshcreative@users.noreply.github.com>
2026-07-18 10:08:16 +0200

    Initial commit

---
+++ resources/views/woocommerce/emails/customer-failed-order.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Customer failed order email
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-failed-order.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 10.4.0
+ */
+
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$email_improvements_enabled = FeaturesUtil::feature_is_enabled( 'email_improvements' );
+
+/**
+ * Hook for the woocommerce_email_header.
+ *
+ * @hooked WC_Emails::email_header() Output the email header
+ * @since 3.7.0
+ */
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<p>Ta wiadomość jest generowana automatycznie, prosimy na nią nie odpowiadać</p><br><br>
+<p>Dzień dobry,</p>
+<p>Ze względu na brak opłaty poniższe zamówienie zostało anulowane.</p>
+
+<?php
+/**
+ * Hook for the woocommerce_email_order_details.
+ *
+ * @hooked WC_Emails::order_details() Shows the order details table.
+ * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
+ * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
+ * @since 2.5.0
+ */
+do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * Hook for the woocommerce_email_order_meta.
+ *
+ * @hooked WC_Emails::order_meta() Shows order meta data.
+ * @since 1.0.0
+ */
+do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * Hook for woocommerce_email_customer_details.
+ *
+ * @hooked WC_Emails::customer_details() Shows customer details
+ * @hooked WC_Emails::email_address() Shows email address
+ * @since 1.0.0
+ */
+do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * Show user-defined additional content - this is set in each email's settings.
+ */
+if ( $additional_content ) {
+	echo $email_improvements_enabled ? '<table border="0" cellpadding="0" cellspacing="0" width="100%" role="presentation"><tr><td class="email-additional-content">' : '';
+	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+	echo $email_improvements_enabled ? '</td></tr></table>' : '';
+}
+
+/**
+ * Hook for the woocommerce_email_footer.
+ *
+ * @hooked WC_Emails::email_footer() Output the email footer
+ * @since 3.7.0
+ */
+do_action( 'woocommerce_email_footer', $email );
